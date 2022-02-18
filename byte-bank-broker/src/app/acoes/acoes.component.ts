93c086ba668a83032fa1b6df71cc7a32502d5eb6
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-02-18 16:19:15 -0300

    add/ observable

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { AcoesService } from './acoes.service';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Acoes } from './modelo/acoes'
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent {
+export class AcoesComponent implements OnInit{
   acoesInput = new FormControl();
+  acoes: Acoes;
 
-  constructor() {}
+  constructor(private acoesService: AcoesService) {}
+
+  ngOnInit(): void {
+      this.acoesService.getAcoes().subscribe((retornoAPI) => {
+        this.acoes = retornoAPI.payload;
+      })
+  }
 }
