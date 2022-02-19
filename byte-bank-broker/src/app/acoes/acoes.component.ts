ca8f7fe9d7d41064fca5517e55f2518b67bd13a0
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-02-19 11:44:03 -0300

    rxjs library

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,22 +1,38 @@
 import { AcoesService } from './acoes.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Acoes } from './modelo/acoes'
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
+import { merge } from 'rxjs';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit{
+export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes: Acoes;
+  
+  todasAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => { console.log('Fluxo inicial') })
+  );
+  
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
+    tap(() => {
+      console.log("Fluxo do filtro")
+    }),
+    tap(console.log),
+    filter(
+      (valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length
+    ),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+    )
+
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
 
   constructor(private acoesService: AcoesService) {}
-
-  ngOnInit(): void {
-      this.acoesService.getAcoes().subscribe((retornoAPI) => {
-        this.acoes = retornoAPI.payload;
-      })
-  }
-}
+  
+ }
--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,6 +1,8 @@
+import { Acao, Acoes, AcoesAPI } from './modelo/acoes';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, pluck } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,25 @@ export class AcoesService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getAcoes(): Observable<any>{
-    return this.httpClient.get<any>('http://localhost:3000/acoes');
+  getAcoes(valor?: string) {
+    const params = valor ? new HttpParams().append('valor', valor) : undefined; 
+    return this.httpClient.get<AcoesAPI>('http://localhost:3000/acoes', { params }).pipe(
+    pluck('payload'),
+    map((acoes) => acoes.sort((acaoA, acaoB) => {
+          return this.ordenaPorCodigo(acaoA, acaoB);
+        })
+      )
+    );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    } 
+    if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    } 
+    return 0;
   }
 
 }
